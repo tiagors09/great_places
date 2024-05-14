cbed95bee8f3131f3d4418cbfc33cf2f95d4f078
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-14 10:59:38 -0300

    feat(models): :sparkles: Added attrs to PlaceLocation
    Added latitude and longitude attrs

--- lib/model/place.dart
+++ lib/model/place.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
 
-class PlaceLocation {}
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+
+  PlaceLocation(this.latitude, this.longitude);
+}
 
 class Place {
   final String id;
