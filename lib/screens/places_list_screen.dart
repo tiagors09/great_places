89d2ff3c3d7f6acccc83755b3b1d231cb4b94b77
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-14 10:19:07 -0300

    feat(screens): Add stateless widget code

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
