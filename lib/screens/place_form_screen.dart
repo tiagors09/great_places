978f4cb2f7c100dfbe299afcab158917eccfb685
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-14 10:43:28 -0300

    feat(screens): :sparkles: Add StatefulWidget base code

---
+++ lib/screens/place_form_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({super.key});
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
