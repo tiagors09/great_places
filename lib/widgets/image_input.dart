7183959c4199c03a7d69ef1c0864e63ccd6d6380
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-15 09:45:41 -0300

    feat(widgets): :sparkles: Add container to show selected image and a text button to get a pic

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -10,6 +10,28 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          margin: const EdgeInsets.only(
+            bottom: 10,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: const Text('Nenhuma imagem!'),
+        ),
+        TextButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.camera),
+          label: const Text('Tirar Foto'),
+        ),
+      ],
+    );
   }
 }
