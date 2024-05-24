8379fc2ad0c3fbc77a303edd4b15952b73cbd722
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-24 08:58:05 -0300

    feat(providers): :sparkles: Add functions to insert and load places

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -2,11 +2,28 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:great_places/utils/db_util.dart';
 
 import '../model/place.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
+
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map(
+          (data) => Place(
+            data['id'],
+            data['title'],
+            PlaceLocation(0, 0, ''),
+            File(data['image']),
+          ),
+        )
+        .toList();
+
+    notifyListeners();
+  }
 
   List<Place> get items {
     return [..._items];
@@ -29,6 +46,14 @@ class GreatPlaces with ChangeNotifier {
     );
 
     _items.add(newPlace);
+    DbUtil.insert(
+      'places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+      },
+    );
     notifyListeners();
   }
 }
