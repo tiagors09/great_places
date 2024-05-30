fd7afed4156534080999a36242cbca7ebcd853ae
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-30 13:39:27 -0300

    feat(utils): :sparkles: Add method to get addres form position

--- lib/utils/location_util.dart
+++ lib/utils/location_util.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
 const googleApiKey = String.fromEnvironment('GOOGLE_API_KEY');
 
 class LocationUtil {
@@ -6,5 +11,16 @@ class LocationUtil {
     required double longitude,
   }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=12&size=400x400&key=$googleApiKey';
+  }
+
+  static Future<String> getAdressFrom(LatLng position) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$googleApiKey';
+
+    final response = await http.get(
+      Uri.parse(url),
+    );
+
+    return jsonDecode(response.body)['results'][0]['formatted_address'];
   }
 }
